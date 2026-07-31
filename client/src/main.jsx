50fb5909d04e3461d8699db664f3df2627c532f9
Mathmindsetoffical160 <63045141-mathmindsetoffi@users.noreply.replit.com>
2026-07-31 17:34:45 +0000

    revert: restore original frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import AppErrorBoundary from './components/AppErrorBoundary';
 import './index.css';
 
-const root = document.getElementById('root');
-const bootFallback = document.getElementById('boot-fallback');
-if (bootFallback) bootFallback.remove();
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppErrorBoundary>
-        <App />
-      </AppErrorBoundary>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
